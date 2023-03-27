dc780aa239dfe09fa4926b108025c8842be2844d
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-03-27 22:13:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,69 +2,98 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Підключення до БД
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'ukd_student',
-  host: '0.tcp.ngrok.io',
-  database: 'ukd_db',
-  password: 'password',
-  port: 11439,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'users',
+  password: '0808',
+  port: 5432
 })
 
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) throw error
+app.get('/', (req, res) => {
+  res.send('Hi')
+})
 
-    response.status(200).json(results.rows)
-  })
-}
+app.post('/students', (req, res) => {
+  const { name, email } = req.body;
 
-// Створення таблиці
-const createTable = (request, response) => {
   pool.query(
-    `CREATE TABLE IF NOT EXISTS "students_Roketskyi" (
-      id SERIAL PRIMARY KEY,
-      first_name VARCHAR(255),
-      last_name VARCHAR(255),
-      age INTEGER
-    );`,
-
+    'INSERT INTO students (name, email) VALUES ($1, $2)',
+    [name, email],
     (error, results) => {
-      if (error) throw error;
-
-      response.status(200).json(results.rows)
-      console.log('Таблиця успішно створена');
+      if (error) {
+        res.status(500).send(error.message);
+      } else {
+        res.status(201).send('Student added successfully');
+      }
     }
   );
-}
+});
 
+app.post('/tasks', (req, res) => {
+  const { title, description, student_id, subject_id } = req.body;
 
-// Створення юзера
-const createUser = (request, response) => {
-  const { first_name, last_name, age } = { first_name: 'roman', last_name: 'roket', age: 12}
-
-  pool.query(`INSERT INTO students_Roketskyi (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *`, 
-  [first_name, last_name, age], (error, results) => {
-    if (error) {
-      throw error
+  pool.query(
+    'INSERT INTO tasks (title, description, student_id, subject_id) VALUES ($1, $2, $3, $4)',
+    [title, description, student_id, subject_id],
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else {
+        res.status(201).send('Task added successfully');
+      }
     }
-
-    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-  })
-}
-
-app.get('/', (req, res) => {
-  getUsers(req, res);
+  );
 });
 
-app.get('/createUser', (req, res) => {
-  createUser(req, res);
+app.get('/students/tasks', (req, res) => {
+  pool.query(
+    'SELECT students.id, students.name, students.email, tasks.id as task_id, tasks.title as task_title, tasks.description as task_description, subjects.name as subject_name FROM students LEFT JOIN tasks ON students.id = tasks.student_id LEFT JOIN subjects ON tasks.subject_id = subjects.id ORDER BY students.id',
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else {
+        const students = [];
+
+        results.rows.forEach((row) => {
+          const student = students.find((s) => s.id === row.id);
+
+          if (!student) {
+            students.push({
+              id: row.id,
+              name: row.name,
+              email: row.email,
+              tasks: [],
+            });
+          }
+
+          if (row.task_id) {
+            students[students.length - 1].tasks.push({
+              id: row.task_id,
+              title: row.task_title,
+              description: row.task_description,
+              subject_name: row.subject_name,
+            });
+          }
+        });
+
+        res.send(students);
+      }
+    }
+  );
 });
 
-app.get('/create', (req, res) => {
-  createTable(req, res);
-});
 
 app.listen(port, () => {
-    console.log(`Веб-сервер був запущений на порту ${port}`);
+  console.log(`Веб-сервер був запущений за наступним посиланням: http://localhost:${port}/`);
+
+  pool.connect((error) => {
+    if (error) {
+      console.error(`Error connecting to database ${error}`);
+    } else {
+      console.log('Connected to database');
+    }
+  });
 });
